7d53e57760bac11a662c2acf18c1a5f7a540ce40
fwas <fwas.corp@gmail.com>
2023-11-14 19:42:13 +0100

    visually functional checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
 import Container from './components/Container/Container';
+import Checkbox from './components/Filters/Checkbox';
 
 const App = () => {
    return (
       <Container>
          <h1>JoBoard 🛹</h1>
+         <Checkbox text={'Temporary text'} />
+         <Checkbox text={'Temporary text2'} />
       </Container>
    );
 };
---
+++ src/components/Filters/Checkbox.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import styles from './Checkbox.module.scss';
+import Checkmark from '../icons/Checkmark';
+import classNames from 'classnames';
+
+interface CheckboxProps {
+   text: string;
+}
+
+const Checkbox = (props: CheckboxProps) => {
+   const [checked, setChecked] = useState(true);
+
+   const rectangleStyle = classNames(
+      styles.checkboxRectangle,
+      checked ? styles.checkboxChecked : '',
+   );
+   const textStyle = classNames(
+      styles.mainTextStyle,
+      checked ? styles.checkedText : styles.uncheckedText,
+   );
+
+   const handleCheck = () => {
+      setChecked(!checked);
+   };
+   return (
+      <div className={styles.checkboxTopWrap} onClick={handleCheck}>
+         <div className={rectangleStyle}>{checked && <Checkmark />}</div>
+         <div className={textStyle}>{props.text}</div>
+      </div>
+   );
+};
+
+export default Checkbox;
