f592e9a4a0d00fc7a5777b55374f32064c7f3d27
meczajkowski <michal.erik.czajkowski@gmail.com>
2023-11-21 00:14:59 +0100

    feat: fetch job offers from api - move query to JobOffersScreen, handle loading & error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useQuery } from '@tanstack/react-query';
-import { fetchJobOffers } from './helpers/api';
 import './App.module.scss';
 
 // components
@@ -7,16 +5,9 @@ import Container from './components/Container/Container';
 import JobOffersScreen from './components/JobOffers/JobOffersScreen';
 
 const App = () => {
-   const query = useQuery({
-      queryKey: ['jobOffers'],
-      queryFn: fetchJobOffers,
-   });
-
-   console.log(query);
-
    return (
       <Container>
-         <JobOffersScreen offers={query.data} />
+         <JobOffersScreen />
       </Container>
    );
 };
--- src/components/JobOffers/JobOffersScreen.tsx
+++ src/components/JobOffers/JobOffersScreen.tsx
@@ -1,23 +1,41 @@
 import styles from './JobOffersScreen.module.scss';
-import { JobData } from '../../types';
 import { IconType } from '../../enums';
+import { useQuery } from '@tanstack/react-query';
+import fetchJobOffers from '../../helpers/api';
 
 // components
 import JobOffersList from './JobOffersList';
 import Input from '../UI/Input';
 
-interface JobOffersProps {
-   offers: JobData[];
-}
+interface JobOffersProps {}
 
 const JobOffersScreen = (props: JobOffersProps) => {
+   const { data, error, isFetching, isPending, isError } = useQuery({
+      queryKey: ['jobOffers'],
+      queryFn: fetchJobOffers,
+   });
    return (
       <section className={styles.screen}>
-         <form className={styles.form}>
-            <Input icon={IconType.Search} placeholder="Search for" />
-            <Input icon={IconType.Location} placeholder="Search location" />
-         </form>
-         <JobOffersList offers={props.offers} />
+         <section>
+            <form className={styles.form}>
+               <Input icon={IconType.Search} placeholder="Search for" />
+               <Input icon={IconType.Location} placeholder="Search location" />
+            </form>
+         </section>
+         <section>
+            {isPending ? (
+               'Loading...'
+            ) : isError ? (
+               <span>Error: {error.message}</span>
+            ) : (
+               <>
+                  {/* this is just for debugging i guess */}
+                  <div>{isFetching ? 'Background Updating...' : ' '}</div>
+
+                  <JobOffersList offers={data} />
+               </>
+            )}
+         </section>
       </section>
    );
 };
