d78f3f1350595a657da2d00843699159f42af4c9
meczajkowski <michal.erik.czajkowski@gmail.com>
2023-11-26 21:27:42 +0100

    feat: job offer preview modal UI - fix CloseIcon svg typo

--- src/components/icons/CloseIcon.tsx
+++ src/components/icons/CloseIcon.tsx
@@ -15,8 +15,8 @@ const CloseIcon = (props: CloseIconProps) => {
          fill="none"
       >
          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
+            fillRule="evenodd"
+            clipRule="evenodd"
             d="M18.7927 19.5002L12.4292 25.8637L13.1363 26.5708L19.4998 20.2074L25.8637 26.5713L26.5708 25.8642L20.2069 19.5002L26.5714 13.1358L25.8643 12.4287L19.4998 18.7931L13.1358 12.4291L12.4287 13.1362L18.7927 19.5002Z"
             fill="#545454"
          />
