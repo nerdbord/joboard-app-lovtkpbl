ec8de4baf95355a2bf0721c9eee6d3c10102547a
meczajkowski <michal.erik.czajkowski@gmail.com>
2023-11-26 21:26:06 +0100

    feat: job offer preview modal UI - add modal header styles

--- src/components/JobOfferPreview/JobOfferPreview.tsx
+++ src/components/JobOfferPreview/JobOfferPreview.tsx
@@ -8,7 +8,27 @@ interface JobOfferPreviewProps {
 }
 
 const JobOfferPreview = (props: JobOfferPreviewProps) => {
-   return <Modal onClose={props.onClose}>JobOfferPreview</Modal>;
+   return (
+      <Modal onClose={props.onClose}>
+         <section className={styles.header}>
+            <div className={styles.imageContainer}>
+               <img src="https://i.imgur.com/yaKYWeN.png" alt="jobtitle" />
+            </div>
+            <div className={styles.headerText}>
+               <h4>Java Developer | Greenfield, Microservices</h4>
+               <span>JAVASCRIPT・NEST・TYPESCRIPT・REACT</span>
+            </div>
+         </section>
+
+         {/* <section>
+            <section>
+               <h5>Transportation System Support IT Specialist (Core IT Team)</h5>
+               <article></article>
+            </section>
+            <section></section>
+         </section> */}
+      </Modal>
+   );
 };
 
 export default JobOfferPreview;
