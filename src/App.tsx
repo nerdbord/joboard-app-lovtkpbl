46d0bc281fa0ee7ec44ac4974ddae21e59fce463
meczajkowski <michal.erik.czajkowski@gmail.com>
2023-12-03 01:55:46 +0100

    feat: adjust job offers list to mobile - add header mobile styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import styles from './App.module.scss';
 // components
-import Container from './components/Container/Container';
 import Filters from './components/Filters/Filters';
 import JobOffersScreen from './components/JobOffers/JobOffersScreen';
 import { FiltersProvider } from './components/Filters/FilterContext';
 
 const App = () => {
    return (
-      <Container>
-         <FiltersProvider>
-            <div className={styles.mainScreen}>
-               <section className={styles.filtersSection}>
-                  <h1>👾 JO-BOARD</h1>
-                  <Filters />
-               </section>
-               <JobOffersScreen />
-            </div>
-         </FiltersProvider>
-      </Container>
+      <FiltersProvider>
+         <div className={styles.mainScreen}>
+            <section className={styles.filtersSection}>
+               <h1>👾 JO-BOARD</h1>
+               <Filters />
+            </section>
+            <JobOffersScreen />
+         </div>
+      </FiltersProvider>
    );
 };
 
