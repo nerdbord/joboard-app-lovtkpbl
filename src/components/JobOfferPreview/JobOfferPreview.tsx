98730862357f915b62bec5ec314989beabcc913c
fwas <fwas.corp@gmail.com>
2023-12-26 12:32:31 +0100

    divided element sections into separate elements

--- src/components/JobOfferPreview/JobOfferPreview.tsx
+++ src/components/JobOfferPreview/JobOfferPreview.tsx
@@ -24,6 +24,108 @@ const JobOfferPreview = (props: JobOfferPreviewProps) => {
    const salaryToFormatted =
       data && data.salaryTo.toLocaleString('en-US', { useGrouping: true }).replace(/,/g, ' ');
 
+   const HeaderSection = () => {
+      if (!data) {
+         return;
+      } else
+         return (
+            <section className={styles.headerSection}>
+               <div className={styles.imageContainer}>
+                  <img src={data.image} alt={data.title} />
+               </div>
+               <div className={styles.headerText}>
+                  <h4>{data.title}</h4>
+                  <span>{data.technologies.join('・')}</span>
+               </div>
+            </section>
+         );
+   };
+
+   const DescriptionBox = () => {
+      if (!data) {
+         return;
+      } else
+         return (
+            <div className={styles.descriptionBox}>
+               <section className={classNames(styles.sectionBg, styles.descriptionSection)}>
+                  <h5>{data.title}</h5>
+                  <article className={styles.jobDescription}>{data.description}</article>
+               </section>
+            </div>
+         );
+   };
+
+   const ButtonSection = () => {
+      if (!data) {
+         return;
+      } else
+         return (
+            <section className={classNames(styles.sectionBg, styles.btnSection)}>
+               <Button
+                  type={ButtonType.Primary}
+                  className={styles.button}
+                  onClick={() => {
+                     window.location.href = data.offerUrl;
+                  }}
+               >
+                  Visit offer ➔
+               </Button>
+            </section>
+         );
+   };
+
+   const InfoList = () => {
+      if (!data) {
+         return;
+      } else
+         return (
+            <ul className={classNames(styles.sectionBg, styles.infoList)}>
+               <li className={styles.infoItem}>
+                  <h6 className={styles.infoItemTitle}>Added</h6>
+                  <span className={styles.infoItemValue}>
+                     {formatTimeDifference(data.createdAt)}
+                  </span>
+               </li>
+               <hr />
+               <li className={styles.infoItem}>
+                  <h6 className={styles.infoItemTitle}>Company</h6>
+                  <span className={styles.infoItemValue}>{data.companyName}</span>
+               </li>
+               <hr />
+               <li className={styles.infoItem}>
+                  <h6 className={styles.infoItemTitle}>Seniority</h6>
+                  <span className={styles.infoItemValue}>{data.seniority}</span>
+               </li>
+               <hr />
+               <li className={styles.infoItem}>
+                  <h6 className={styles.infoItemTitle}>Location</h6>
+                  <div>
+                     <p className={styles.infoItemValue}>{data.city},</p>
+                     <p className={styles.infoItemValue}>{data.country}</p>
+                  </div>
+               </li>
+               <hr />
+               <li className={styles.infoItem}>
+                  <h6 className={styles.infoItemTitle}>Job type</h6>
+                  <span className={styles.infoItemValue}>{data.workLocation}</span>
+               </li>
+               <hr />
+               <li className={styles.infoItem}>
+                  <h6 className={styles.infoItemTitle}>Contract</h6>
+                  <span className={styles.infoItemValue}>{data.jobType}</span>
+               </li>
+               <hr />
+               <li className={styles.infoItem}>
+                  <h6 className={styles.infoItemTitle}>Salary</h6>
+                  <span className={styles.infoItemValue}>
+                     {' '}
+                     {`${salaryFromFormatted} - ${salaryToFormatted} ${data.currency} net`}
+                  </span>
+               </li>
+            </ul>
+         );
+   };
+
    return (
       <Modal onClose={props.onClose}>
          {isPending ? (
@@ -34,81 +136,12 @@ const JobOfferPreview = (props: JobOfferPreviewProps) => {
             data && (
                <>
                   {/* <Loader /> */}
-                  <section className={styles.headerSection}>
-                     <div className={styles.imageContainer}>
-                        <img src={data.image} alt={data.title} />
-                     </div>
-                     <div className={styles.headerText}>
-                        <h4>{data.title}</h4>
-                        <span>{data.technologies.join('・')}</span>
-                     </div>
-                  </section>
-
+                  <HeaderSection />
                   <section className={styles.detailsSection}>
-                     <div className={styles.descriptionBox}>
-                        <section
-                           className={classNames(styles.sectionBg, styles.descriptionSection)}
-                        >
-                           <h5>{data.title}</h5>
-                           <article className={styles.jobDescription}>{data.description}</article>
-                        </section>
-                     </div>
+                     <DescriptionBox />
                      <section className={styles.infoSection}>
-                        <section className={classNames(styles.sectionBg, styles.btnSection)}>
-                           <Button
-                              type={ButtonType.Primary}
-                              className={styles.button}
-                              onClick={() => {
-                                 window.location.href = data.offerUrl;
-                              }}
-                           >
-                              Visit offer ➔
-                           </Button>
-                        </section>
-                        <ul className={classNames(styles.sectionBg, styles.infoList)}>
-                           <li className={styles.infoItem}>
-                              <h6 className={styles.infoItemTitle}>Added</h6>
-                              <span className={styles.infoItemValue}>
-                                 {formatTimeDifference(data.createdAt)}
-                              </span>
-                           </li>
-                           <hr />
-                           <li className={styles.infoItem}>
-                              <h6 className={styles.infoItemTitle}>Company</h6>
-                              <span className={styles.infoItemValue}>{data.companyName}</span>
-                           </li>
-                           <hr />
-                           <li className={styles.infoItem}>
-                              <h6 className={styles.infoItemTitle}>Seniority</h6>
-                              <span className={styles.infoItemValue}>{data.seniority}</span>
-                           </li>
-                           <hr />
-                           <li className={styles.infoItem}>
-                              <h6 className={styles.infoItemTitle}>Location</h6>
-                              <div>
-                                 <p className={styles.infoItemValue}>{data.city},</p>
-                                 <p className={styles.infoItemValue}>{data.country}</p>
-                              </div>
-                           </li>
-                           <hr />
-                           <li className={styles.infoItem}>
-                              <h6 className={styles.infoItemTitle}>Job type</h6>
-                              <span className={styles.infoItemValue}>{data.workLocation}</span>
-                           </li>
-                           <hr />
-                           <li className={styles.infoItem}>
-                              <h6 className={styles.infoItemTitle}>Contract</h6>
-                              <span className={styles.infoItemValue}>{data.jobType}</span>
-                           </li>
-                           <hr />
-                           <li className={styles.infoItem}>
-                              <h6 className={styles.infoItemTitle}>Salary</h6>
-                              <span className={styles.infoItemValue}>
-                                 {' '}
-                                 {`${salaryFromFormatted} - ${salaryToFormatted} ${data.currency} net`}
-                              </span>
-                           </li>
-                        </ul>
+                        <ButtonSection />
+                        <InfoList />
                      </section>
                   </section>
                </>
