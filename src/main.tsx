4053cb56d0b36075018d144626963f5c96be83b7
meczajkowski <michal.erik.czajkowski@gmail.com>
2024-01-10 00:18:12 +0100

    feat: deploy - deploy on vercel with analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import fetchJobOffers from './api';
+import { Analytics } from '@vercel/analytics/react';
 
 import './index.scss';
 
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement).render(
    <React.StrictMode>
       <QueryClientProvider client={queryClient}>
          <App />
+         <Analytics />
       </QueryClientProvider>
    </React.StrictMode>,
 );
